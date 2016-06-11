99e4f647f805b2bca6af95e3f9e6e7f22e42228c
Maciek <maciej.harazinski@gmail.com>
2016-06-11 14:12:21 +0200

    euler 4 - palindrome manual check

--- src/pl/euler04/Euler04.java
+++ src/pl/euler04/Euler04.java
@@ -1,13 +1,12 @@
 package pl.euler04;
 
-
 public class Euler04 {
 
     public int generateMultiples() {
         int max = 1;
             for (int i = 999; i > 0; i--) {
                 for (int y = 999; y >= 0; y--) {
-                    if (isPalindrome(i*y)) {
+                    if (isPalindromeManual(i*y)) {
                         if (i*y > max) {
                             max = i*y;
                         }
@@ -31,6 +30,22 @@ public class Euler04 {
         {
             return false;
         }
+    }
 
+    public boolean isPalindromeManual(long number) {
+        String str = Long.toString(number);
+        String rev = "";
+
+        for (int i = str.length()-1; i>=0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if (str.equals(rev)) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
